a294d67876591dc84a8744e1b4a468c9ee70212c
sparsh singh chundawat <singhsparsh2003@gmail.com>
2024-07-25 21:15:17 +0530

    Fix for Vercel deployment

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const app = express();
 const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
+
+// Create the Express app
+const app = express();
 
 // Create the HTTP server
 const server = http.createServer(app);
@@ -17,25 +19,23 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Socket.IO connection handler
-io.on('connection', function(socket) {
-    socket.on('send-location', function(data) {
+io.on('connection', function (socket) {
+    socket.on('send-location', function (data) {
         io.emit('recieve-location', { id: socket.id, ...data });
     });
 
-    // console.log('Client connected');
+    console.log('Client connected');
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         io.emit('user-disconnected', socket.id);
-        // console.log('Client disconnected');
+        console.log('Client disconnected');
     });
 });
 
 // Handle the root route
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index'); // Make sure there's a file named "index.ejs" in the "views" directory
 });
 
-// Start the server on port 3000
-server.listen(3000, () => {
-    console.log('Server running on 3000');
-});
+// Export the app (not the server) for Vercel
+module.exports = app;
